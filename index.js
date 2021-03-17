5a48cff308618caaa53f9e44158aa8647821ece2
Markus Heine <markus.heine@pinuts.de>
2021-03-17 14:33:51 +0100

    remove unnecessary code

--- index.js
+++ index.js
@@ -2,12 +2,4 @@ module.exports = {
     extends: [
         '@pinuts/eslint-config-pinuts-base'
     ],
-    parserOptions: {
-        ecmaVersion: es2019,
-    },
-    env: {
-        browser: true,
-        es6: true,
-        jquery: true,
-    }
 };
